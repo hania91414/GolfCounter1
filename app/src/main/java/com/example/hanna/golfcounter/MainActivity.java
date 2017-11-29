fec1eddf26005ff94edb75f1c09ba8efd32a4c55
Hanna <hania91414@gmail.com>
2017-11-29 20:10:08 +0100

    updated

--- app/src/main/java/com/example/hanna/golfcounter/MainActivity.java
+++ app/src/main/java/com/example/hanna/golfcounter/MainActivity.java
@@ -8,73 +8,75 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
     int scoreTeamB = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
 
     }
 
     public void addThreeToTeamA(View view) {
-        scoreTeamA = scoreTeamA + 3;
+        scoreTeamA = scoreTeamA +3;
         displayForTeamA(scoreTeamA);
     }
 
     public void addTwoToTeamA(View view) {
-        scoreTeamA = scoreTeamA + 2;
+        scoreTeamA = scoreTeamA +2;
         displayForTeamA(scoreTeamA);
     }
 
     public void addOneToTeamA(View view) {
-        scoreTeamA = scoreTeamA + 1;
+        scoreTeamA = scoreTeamA +1;
         displayForTeamA(scoreTeamA);
     }
 
     public void subsOneTeamA(View view) {
-        scoreTeamA = scoreTeamA - 1;
+        scoreTeamA = scoreTeamA -1;
         displayForTeamA(scoreTeamA);
     }
 
     public void subsTwoTeamA(View view) {
-        scoreTeamA = scoreTeamA - 2;
+        scoreTeamA = scoreTeamA -2;
         displayForTeamA(scoreTeamA);
     }
 
     public void subsThreeTeamA(View view) {
-        scoreTeamA = scoreTeamA - 3;
+        scoreTeamA = scoreTeamA -3;
         displayForTeamA(scoreTeamA);
     }
 
 
     public void addThreeToTeamB(View view) {
-        scoreTeamB = scoreTeamB + 3;
+        scoreTeamB = scoreTeamB +3;
         displayForTeamB(scoreTeamB);
     }
 
     public void addTwoToTeamB(View view) {
-        scoreTeamB = scoreTeamB + 2;
+        scoreTeamB = scoreTeamB +2;
         displayForTeamB(scoreTeamB);
     }
 
     public void addOneToTeamB(View view) {
-        scoreTeamB = scoreTeamB + 1;
+        scoreTeamB = scoreTeamB +1;
         displayForTeamB(scoreTeamB);
     }
 
 
+
     public void subsOneTeamB(View view) {
-        scoreTeamB = scoreTeamB - 1;
+        scoreTeamB = scoreTeamB -1;
         displayForTeamB(scoreTeamB);
     }
 
     public void subsTwoTeamB(View view) {
-        scoreTeamB = scoreTeamB - 2;
+        scoreTeamB = scoreTeamB -2;
         displayForTeamB(scoreTeamB);
     }
 
     public void subsThreeTeamB(View view) {
-        scoreTeamB = scoreTeamB - 3;
+        scoreTeamB = scoreTeamB -3;
         displayForTeamB(scoreTeamB);
     }
 
@@ -87,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
     /**
      * Displays the given score for Team A.
      */
@@ -105,4 +106,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-
